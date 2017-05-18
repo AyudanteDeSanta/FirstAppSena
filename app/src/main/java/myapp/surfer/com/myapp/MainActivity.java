d73b1fb8a1a25dac246967cc4d8b752073f99f3d
Didier Albarrán De La Torre <didier.albarran@outlook.com>
2017-05-17 20:59:50 -0500

    Sensores, values por localización (region)

--- app/src/main/java/myapp/surfer/com/myapp/MainActivity.java
+++ app/src/main/java/myapp/surfer/com/myapp/MainActivity.java
@@ -1,6 +1,11 @@
 package myapp.surfer.com.myapp;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,20 +13,29 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, View.OnClickListener {
     TextView campo_texto;
     EditText edUsuario, edContrasena;
     ImageView ivImagen;
     EditText edDato;
     Button btCambiarDato, bActivity;
+    LinearLayout ln;
+    Sensor sensor;
+    SensorManager sl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ln = (LinearLayout) findViewById(R.id.linear);
+        sl = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor = sl.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        sl.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
 
         campo_texto = (TextView) findViewById(R.id.tvTexto);
         campo_texto.setText("Hola de nuevo Didier");
@@ -84,5 +98,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
         }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        String texto = String.valueOf(event.values[0]);
+        campo_texto.setText(texto);
+
+        float retorno = Float.parseFloat(texto);
+        if (retorno==0)
+        {
+            ln.setBackgroundColor(Color.BLUE);
+        }
+        else
+        {
+            ln.setBackgroundColor(Color.RED);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
